86350b0855ae2c374f7dc001486ae7e6cc49476e
Thomas905 <thomasclarisse03@gmail.com>
2022-12-20 15:24:49 +0100

    add confetti

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 
 import { ScratchCard, SCRATCH_TYPE } from "scratchcard-js";
+import confetti from "canvas-confetti";
+
 
 const scContainer = document.getElementById("js--sc--container");
 const sc = new ScratchCard("#js--sc--container", {
@@ -8,7 +10,7 @@ const sc = new ScratchCard("#js--sc--container", {
     containerWidth: scContainer.offsetWidth,
     containerHeight: 300,
     imageForwardSrc:
-        "https://i.imgur.com/rgvJWfO.jpg",
+        "https://i.imgur.com/IC1KpJp.jpg",
     htmlBackground: '' +
         '<div class="sc__html-background">' +
         '   <div class="sc__html-background__title">Bravo !</div>' +
@@ -17,6 +19,10 @@ const sc = new ScratchCard("#js--sc--container", {
     clearZoneRadius: 30,
     percentToFinish: 30, // When the percent exceeds 50 on touchend event the callback will be exec.
     callback: function () {
+        confetti({
+            particleCount: 150,
+            spread: 60
+        });
         stop();
     }
 });
