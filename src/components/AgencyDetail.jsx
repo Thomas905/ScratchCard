6013f754a6c6a0ecdea7372a4f4c070d14484f99
Thomas905 <thomasclarisse03@gmail.com>
2023-01-10 14:47:50 +0100

    style

--- src/components/AgencyDetail.jsx
+++ src/components/AgencyDetail.jsx
@@ -4,6 +4,7 @@ import { data1 } from '../assets/json/export.js';
 import { address } from '../assets/json/address.js';
 import ScratchCard from "react-scratchcard";
 import confetti from "canvas-confetti";
+import "../assets/styles/styles.css"
 
 const AgencyDetail = () => {
     const { id } = useParams();
@@ -35,21 +36,21 @@ const AgencyDetail = () => {
     };
     return (
         <div>
+            <img src="https://imgur.com/kydL87p.png" alt="merciii"></img>
             <nav className="d-flex">
-                <img src="https://imgur.com/kydL87p.png" alt="merciii"></img>
                 <div className="w-100">
-                    <h1 className="text-center pt-3 pb-3">CHALLENGE MERCIII du 1.12.2022 AU 28.02.2023</h1>
+                    <h1 className="text-center pt-3 pb-5 text-warning">Challenge Merciii <br /> <span className="h4 text-black">du 1.12.2022 AU 28.02.2023</span></h1>
                 </div>
             </nav>
             <div className="header d-flex justify-content-center">
                 <img src="https://i.imgur.com/qh8Zmmb.gif" alt="gif" className="gif"></img>
             </div>
-            <h2 className="text-center pt-5" >Bonjour l'équipe { agency.name }</h2>
-            <h3 className="text-center" >Combien de flyers ont été scannées dans votre agence ?</h3>
-            <p className="text-center h3">Grattez ce ticket pour le savoir !</p>
-            <div className="d-flex justify-content-center">
+            <h2 className="text-center pt-5" >Bonjour l'équipe { agency.name },</h2>
+            <h3 className="text-center" >combien de flyers ont été scannées dans votre agence ?<br /><br /></h3>
+            <p className="text-center h3"><i className="text-success">Grattez ce ticket pour le savoir !</i></p>
+            <div className="d-flex justify-content-center p-5">
                 <ScratchCard {...settings} >
-                    <div className="h1 text-center m-5">{ data.length } flyers ont été scannés à date !</div>
+                    <div className="h1 text-center m-5"><p className="text-warning">{ data.length }</p> flyers ont été scannés à date !</div>
                 </ScratchCard>
             </div>
         </div>
